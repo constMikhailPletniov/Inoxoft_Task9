6d58f031e56c6a8e92123daecb05d5850858c67e
constMikhailPletniov <mihailpl1995@gmail.com>
2021-09-09 21:46:04 +0300

    I've done task 9

--- controls/admin.control.js
+++ controls/admin.control.js
@@ -2,12 +2,12 @@ const { statusEnum, userRoleEnum } = require('../config');
 const { SuperAdmin } = require('../database');
 const { pasService } = require('../services');
 module.exports = {
-    createSuperAdmin: async ({ name, email = statusEnum.EMAIL, role, password = statusEnum.EMAIL_PASSWORD }, req, res) => {
+    createSuperAdmin: async (req, res, next) => {
         try {
             const admin = await SuperAdmin.create({
-                name,
+                name: statusEnum.SUPER_ADMIN_NAME,
                 email: statusEnum.EMAIL,
-                role,
+                role: userRoleEnum.SUPER_ADMIN,
                 password: statusEnum.EMAIL_PASSWORD
             });
             const passHash = await pasService.hash(admin.password);
@@ -17,22 +17,6 @@ module.exports = {
             });
 
             res.status(statusEnum.OK).json('Create SuperAdmin');
-        } catch (err) {
-            res.json(err);
-        }
-    },
-    checkSuperAdmin: async (req, res, next) => {
-        try {
-            const superAdmin = await SuperAdmin.findOne({ role: userRoleEnum.SUPER_ADMIN });
-            console.log(superAdmin);
-            if (!superAdmin) {
-                await createSuperAdmin({
-                    name: statusEnum.SUPER_ADMIN_NAME,
-                    email: statusEnum.EMAIL,
-                    role: userRoleEnum.SUPER_ADMIN,
-                    password: statusEnum.EMAIL_PASSWORD
-                });
-            }
         } catch (err) {
             next(err);
         }
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,4 +1,5 @@
 module.exports = {
+    adminMiddleware: require('./admin.middleware'),
     authMiddleWare: require('./auth.middlewares'),
     pestMiddleWare: require('./pest.middlewares'),
     userMiddleWare: require('./user.middlewares')
